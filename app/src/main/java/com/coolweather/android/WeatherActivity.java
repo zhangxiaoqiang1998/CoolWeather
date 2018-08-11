1eaab83eecb71b52593ddf05bdbb8aaadb9ef798
zxq <977840463@qq.com>
2018-08-11 10:53:27 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.coolweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -23,6 +24,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolweather.android.gson.Forecast;
 import com.coolweather.android.gson.Weather;
+import com.coolweather.android.service.AutoUpdateService;
 import com.coolweather.android.util.HttpUtil;
 import com.coolweather.android.util.Utility;
 
@@ -50,7 +52,7 @@ public class WeatherActivity extends AppCompatActivity {
     public SwipeRefreshLayout swipeRefresh;
     public DrawerLayout drawerLayout;
     private Button navButton;
-    private String mweatherId;//修复更换城市后刷新仍未原城市数据
+    private String mweatherId;//修复更换城市后刷新仍为原城市数据
 
 
     @Override
@@ -172,7 +174,6 @@ public class WeatherActivity extends AppCompatActivity {
         String weatherInfo = weather.now.More.info;
         titleCity.setText(cityName);
         titleUpdateTime.setText(updateTime);
-        Log.d("fuck",degree);
         degreeText.setText(degree);
         weatherInfoText.setText(weatherInfo);
         forecastLayout.removeAllViews();
@@ -203,6 +204,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     /**
