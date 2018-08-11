3d1e06c2342da981c0f742d17132b5b5ed74bc77
zxq <977840463@qq.com>
2018-08-11 12:45:52 +0800

    更改图标和名称

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -61,7 +61,7 @@ public class WeatherActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT>=21){
             View decorView = getWindow().getDecorView();
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            getWindow().setStatusBarColor(Color.argb(100,23,24,25));
         }
         setContentView(R.layout.activity_weather);
         //初始化控件
